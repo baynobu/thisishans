3763bdd23532971049c18f5cadd37902b1f1a55b
baynobu <darmawanhanif30@gmail.com>
2023-10-31 15:38:13 +0700

    update: delete Link tag

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -32,18 +32,6 @@ const HeroSection = () => {
 
         </div>
       </div>
-      <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          <HiArrowDown size={35} className="animate-bounce" />
-        </Link>
-      </div>
     </section>
   )
 }
